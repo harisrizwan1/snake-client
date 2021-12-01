53e23404572e1c16b60564d6b4d0277793002d0d
harisrizwan1 <harisrizwan985@gmail.com>
2021-12-01 16:10:40 -0600

    addes user input

---
+++ input.js
@@ -0,0 +1,32 @@
+let connection;
+
+const setupInput = function(conn) {
+  const stdin = process.stdin;
+  connection = conn;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  switch (key) {
+  case "w":
+    connection.write("Move: up");
+    break;
+  case "s":
+    connection.write("Move: down");
+    break;
+  case "a":
+    connection.write("Move: left");
+    break;
+  case "d":
+    connection.write("Move: right");
+    break;
+  case "\u0003":
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
