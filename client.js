66880da27722af1bdf9fa371cb93bd8e192b7e9f
harisrizwan1 <harisrizwan985@gmail.com>
2021-12-01 15:22:36 -0600

    did next thing

--- client.js
+++ client.js
@@ -2,11 +2,16 @@ const net = require("net");
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '10.0.0.209',
+    host: '172.28.156.116',
     port: 50541
   });
 
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("We're in, chief");
+    conn.write("Name: HR");
+  });
 
   conn.on("data", (data) => {
     console.log(data);
