37ced5489dd0ce7241098c78f7c9d5763ed9c3c0
harisrizwan1 <harisrizwan985@gmail.com>
2021-12-01 15:02:53 -0600

    added client file

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '10.0.0.209',
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
